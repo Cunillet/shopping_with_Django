ff33876bdedc8b19185dbdc0aabb74ac3c72fefe
ORIOL CUNILL <or.cunill@ext.desigual.com>
2020-08-17 14:55:50 +0200

    add functional APIs with basic templates

--- shopping_site/more_polls/views.py
+++ shopping_site/more_polls/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+#from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("This is another index manually created")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'more_polls/index.html', context)
+	#another way would be >>
+	#template = loader.get_template('polls/index.html')
+    #return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	#another way would be >>
+	#try:
+	#    question = Question.objects.get(pk=question_id)
+	#except Question.DoesNotExist:
+	#    raise Http404("Question does not exist")
+	return render(request, 'more_polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
