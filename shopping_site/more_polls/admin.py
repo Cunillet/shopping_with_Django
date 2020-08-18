47d015f0955f1ee09fc7270ec8b352efcbb0b6ce
ORIOL CUNILL <or.cunill@ext.desigual.com>
2020-08-18 10:42:34 +0200

    customize backend administrator

--- shopping_site/more_polls/admin.py
+++ shopping_site/more_polls/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# forced stack visible (displays each field as a new line) >> admin.StackedInline
+# tabular inline (displays more compact) >> admin.TabularInline
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	# reorder fields >>
+	#fields = ['pub_date', 'question_text']
+	# reorder fields splitting into fieldsets
+	fieldsets = [
+		(None,               {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date']}),
+	]
+	# add choices in question management
+	inlines = [ChoiceInline]
+	# specify what to show on admin model elements view
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	# add filter sidebar by given fields, in this case, pub_date
+	list_filter = ['pub_date']
+	# add search field to questions list page
+	search_fields = ['question_text']
+	# set elements per page, by default 100
+	list_per_page = 20
+
+### ANOTATION
+# if Choice is added as inline in Questions
+# it can not be displayed as element itself
+
+
+admin.site.register(Question, QuestionAdmin)
